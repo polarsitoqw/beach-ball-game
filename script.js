56370eb9b6cd5239d1941b2b6405b3ccef004308
polarsitoqw <131508735+polarsitoqw@users.noreply.github.com>
2023-04-22 00:55:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,52 @@
-var ball = document.getElementById("ball");
-var score = document.getElementById("score");
-var sonidoRebote = document.getElementById("sonidoRebote");
-var x = window.innerWidth / 2;
-var y = window.innerHeight / 2;
-var dx = 4; // Aumenta la velocidad en el eje x
-var dy = 4; // Aumenta la velocidad en el eje y
-var scoreValue = 0;
+// Crear un contexto de audio
+var contextoAudio = new (window.AudioContext || window.webkitAudioContext)();
 
-// Función para mover la pelota
-function moveBall() {
-	x += dx;
-	y += dy;
+// Cargar los archivos de sonido
+var sonidoRebote = new Audio('rebote.mp3');
+var sonidoAmbiental = new Audio('playa.mp3');
 
-	if (x + ball.clientWidth / 2 > window.innerWidth || x - ball.clientWidth / 2 < 0) {
-		dx = -dx;
-	}
+// Definir las propiedades de la pelota
+var x = lienzo.width / 2;
+var y = lienzo.height / 2;
+var radio = 20;
+var velocidadX = 2;
+var velocidadY = -2;
 
-	if (y + ball.clientHeight / 2 > window.innerHeight || y - ball.clientHeight / 2 < 0) {
-		dy = -dy;
-	}
+// Contador
+var contador = 0;
 
-	ball.style.left = x - ball.clientWidth / 2 + "px";
-	ball.style.top = y - ball.clientHeight / 2 + "px";
-
-	if (x - ball.clientWidth / 2 <= 0 && y - ball.clientHeight / 2 <= 0) {
-		scoreValue += 1;
-		score.textContent = scoreValue;
-	}
-
-	window.requestAnimationFrame(moveBall);
+// Función para actualizar el contador en el HTML
+function actualizarContador() {
+  document.getElementById('contador').textContent = contador;
 }
 
-sonidoRebote.
+// Lógica de movimiento de la pelota
+function moverPelota() {
+  // ... Código para dibujar la pelota, detectar rebotes, etc.
+
+  // Reproducir sonido de rebote cuando la pelota rebota
+  if (rebota) {
+    sonidoRebote.currentTime = 0; // Reiniciar el sonido
+    sonidoRebote.play(); // Reproducir el sonido de rebote
+  }
+
+  
+  }
+
+ 
+// Actualizar el contador
+  contador++;
+  actualizarContador();
+
+  // Llamar a la función de movimiento de la pelota en el siguiente cuadro de animación
+  requestAnimationFrame(moverPelota);
+}
+
+// Iniciar la reproducción del sonido ambiental
+sonidoAmbiental.
 son
-play();
-// Iniciar movimiento de la pelota
-moveBall();
+loop = true; // Repetir el sonido en un bucle
+sonidoAmbiental.play(); // Reproducir el sonido ambiental
+
+// Llamar a la función de movimiento de la pelota para iniciar la animación
+moverPelota();
