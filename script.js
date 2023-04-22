a8209b9de23f905050bcb22870e077e44dab15d9
polarsitoqw <131508735+polarsitoqw@users.noreply.github.com>
2023-04-22 00:26:34 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+var ball = document.getElementById("ball");
+var score = document.getElementById("score");
+
+var x = window.innerWidth / 2;
+var y = window.innerHeight / 2;
+var dx = 4; // Aumenta la velocidad en el eje x
+var dy = 4; // Aumenta la velocidad en el eje y
+var scoreValue = 0;
+
+// Función para mover la pelota
+function moveBall() {
+	x += dx;
+	y += dy;
+
+	if (x + ball.clientWidth / 2 > window.innerWidth || x - ball.clientWidth / 2 < 0) {
+		dx = -dx;
+	}
+
+	if (y + ball.clientHeight / 2 > window.innerHeight || y - ball.clientHeight / 2 < 0) {
+		dy = -dy;
+	}
+
+	ball.style.left = x - ball.clientWidth / 2 + "px";
+	ball.style.top = y - ball.clientHeight / 2 + "px";
+
+	if (x - ball.clientWidth / 2 <= 0 && y - ball.clientHeight / 2 <= 0) {
+		scoreValue += 1;
+		score.textContent = scoreValue;
+	}
+
+	window.requestAnimationFrame(moveBall);
+}
+
+// Iniciar movimiento de la pelota
+moveBall();
